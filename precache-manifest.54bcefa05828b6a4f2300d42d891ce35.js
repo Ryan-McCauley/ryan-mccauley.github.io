ab1af109f341e23203c144c9d44040c6c6912ea2
Majick333 <skyquakesoftware@proton.me>
2023-08-15 17:41:38 -0500

    initial commit

---
+++ precache-manifest.54bcefa05828b6a4f2300d42d891ce35.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a50fb0a88797f1e343",
+    "url": "/static/css/main.cbdfb513.chunk.css"
+  },
+  {
+    "revision": "89a50fb0a88797f1e343",
+    "url": "/static/js/main.89a50fb0.chunk.js"
+  },
+  {
+    "revision": "68d0c551cc34f1b15e0e",
+    "url": "/static/js/1.68d0c551.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b6393181551938107f2fdbda6f823a7",
+    "url": "/static/media/portfolio.1b639318.jpeg"
+  },
+  {
+    "revision": "c547106fd5fa89faa148cc4b8bdc176f",
+    "url": "/static/media/video.c547106f.mp4"
+  },
+  {
+    "revision": "275a2568558839c50e82730bb7a867b1",
+    "url": "/static/media/icon1.275a2568.svg"
+  },
+  {
+    "revision": "976fada21a3501b5a6bd44177bef3897",
+    "url": "/static/media/icon2.976fada2.svg"
+  },
+  {
+    "revision": "fa59da441cf252eb309d874837341bfe",
+    "url": "/static/media/icon3.fa59da44.svg"
+  },
+  {
+    "revision": "8e5ce1d67741a044c724744f35d72915",
+    "url": "/static/media/blog1.8e5ce1d6.png"
+  },
+  {
+    "revision": "2d41d8884f8e7cfdae6dafaf833ff6bc",
+    "url": "/static/media/blog2.2d41d888.png"
+  },
+  {
+    "revision": "d858e3f88fd0a7b086550e084842d0ef",
+    "url": "/static/media/blog3.d858e3f8.png"
+  },
+  {
+    "revision": "bec61c4b873ca154cc2656257127c838",
+    "url": "/static/media/JsChat.bec61c4b.mp4"
+  },
+  {
+    "revision": "8bfd752527a03545f9e05fc65ffa9cd4",
+    "url": "/index.html"
+  }
+];
